1c22acb9d37a21cdb226c4e9359b01cdef8e51f5
Abdulrahman Hanafy <abdulrahman.elhanafy.dev@gmail.com>
2025-07-13 03:46:13 +0300

    Made it function

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,14 +1,16 @@
 const agrCount = process.argv;
 const firstArg = Number(process.argv[2]);
-if (firstArg === NaN || firstArg == 1 || firstArg == 0) {
-    console.log("1");
-}
-else{
-    let fact = 1;
-    for (let i = 2; i <= firstArg; i++) {
-        fact *= i;
+function factorial(num) {
+    if (num === NaN || num == 1 || num == 0) {
+        return("1");
     }
-
-    console.log(fact);
-
+    else{
+        let fact = 1;
+        for (let i = 2; i <= num; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
 }
+
+console.log(factorial(firstArg));
