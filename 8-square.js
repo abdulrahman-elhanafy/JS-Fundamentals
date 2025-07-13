88508797a1f453783c440659e8604a4fe30d6102
Abdulrahman Hanafy <abdulrahman.elhanafy.dev@gmail.com>
2025-07-13 03:13:11 +0300

    Changed the letter from x small to X capital

--- 8-square.js
+++ 8-square.js
@@ -7,7 +7,7 @@ if (!isNaN(num)) {
     for (let i = 0; i < num; i++) {
         let row = "";
         for (let j = 0; j < num; j++) {
-        row += "x";
+        row += "X";
         }
         console.log(row);
     }
